e5fbe36262380b7320cd6337dedfd989f86a0200
Blaze Rowland <Browland.bdm@gmail.com>
2018-05-20 12:12:02 -0500

    Added Sitemap.xml and Theme-color for mobile usres

--- resources/views/partials/_meta.blade.php
+++ resources/views/partials/_meta.blade.php
@@ -4,4 +4,7 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="author" content="Blaze Rowland">
 <meta name="keywords" content="web developer, create a website, portfolio website, web, developer, designer, html, css, php, laravel, javascript, jquery, database, sql, software, engineer"/>
+<meta name="theme-color" content="#00baeb"/>
+<meta name="msapplication-navbutton-color" content="#00baeb">
+<meta name="apple-mobile-web-app-status-bar-style" content="#00baeb">
 @yield('meta_data')
